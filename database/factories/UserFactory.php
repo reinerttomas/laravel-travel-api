1d567a7ff8def5afb3d3bd009d3814ea9c8b5ef0
Tomáš Reinert <reinerttomas@gmail.com>
2024-09-15 19:24:27 +0200

    fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,7 @@ final class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => self::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
