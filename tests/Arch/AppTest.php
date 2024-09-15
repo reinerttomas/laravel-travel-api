951d244d544d2b58d357a0db82b08f2d1869ec11
Tomáš Reinert <reinerttomas@gmail.com>
2024-09-15 18:47:18 +0200

    install testing

---
+++ tests/Arch/AppTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+arch('php')->preset()->php();
+
+arch('security')->preset()->security();
+
+arch('laravel')->preset()->laravel();
+
+arch('strict')->preset()->strict();
