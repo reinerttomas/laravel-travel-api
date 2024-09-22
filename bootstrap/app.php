90d7f1fd2d7979a4f9021a87cfb5ad1aa9e2a0e6
Tomáš Reinert <reinerttomas@gmail.com>
2024-09-22 12:06:24 +0200

    fix not found route without Accept: application/json header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: '/api/v1',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api([
+        $middleware->api(prepend: [
             ForceJsonResponse::class,
         ]);
     })
